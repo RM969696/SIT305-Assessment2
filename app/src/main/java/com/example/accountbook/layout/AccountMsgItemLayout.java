ce3037ba81daad9f988ea325a296a2eed306f1b8
RM969696 <18855808066@163.com>
2020-05-26 15:22:03 +0800

    add functions to edit the detail of each cost

--- app/src/main/java/com/example/accountbook/layout/AccountMsgItemLayout.java
+++ app/src/main/java/com/example/accountbook/layout/AccountMsgItemLayout.java
@@ -7,18 +7,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.accountbook.AccountMsgActivity;
 import com.example.accountbook.Data.Account_info;
-import com.example.accountbook.Helper.CommonHelper;
 import com.example.accountbook.MainActivity;
 import com.example.accountbook.R;
 
 import java.text.SimpleDateFormat;
 
+//账单item 信息类
 public class AccountMsgItemLayout extends ConstraintLayout {
 
     private Context superContext;
@@ -29,14 +30,16 @@ public class AccountMsgItemLayout extends ConstraintLayout {
     private TextView noteTxt;
     private TextView timeTxt;
     private TextView valueTxt;
+    private Button editBtn;
 
+    private MainActivity mainActivity;
 
     public AccountMsgItemLayout(Context context, ViewGroup root, Account_info _info)
     {
-
         super(context);
         self =  LayoutInflater.from(context).inflate(R.layout.account_msg_item,root,false);//第三个参数传入false，当传true时，会有数据赋值失败的情况，false的时候要addview
         root.addView(self);//addView必须要加在这里
+        mainActivity = (MainActivity) context;
         superContext = context;
         info = _info;
 
@@ -44,13 +47,16 @@ public class AccountMsgItemLayout extends ConstraintLayout {
         noteTxt = self.findViewById(R.id.noteTxt);
         timeTxt = self.findViewById(R.id.timeTxt);
         valueTxt = self.findViewById(R.id.ValueTxt);
+        editBtn = self.findViewById(R.id.editBtn);
 
 
         self.setOnClickListener(clickListener);
+        editBtn.setOnClickListener(btnClickListener);
 
         init();
     }
 
+    //初始化，显示数据
     private void init()
     {
         targetNameTxt.setText(info.target);
@@ -59,9 +65,15 @@ public class AccountMsgItemLayout extends ConstraintLayout {
         String timeDesc = format.format(Long.parseLong(info.time));
         timeTxt.setText(timeDesc);
         valueTxt.setText( (info.value<0? "":"+") + info.value );
-        //valueTxt.setTextColor(info.value<0? CommonHelper.GetColorId("colorRed",superContext):CommonHelper.GetColorId("colorGreen",superContext));
         valueTxt.setTextColor(info.value<0? superContext.getResources().getColor(R.color.colorRed):superContext.getResources().getColor(R.color.colorGreen));
     }
+
+    OnClickListener btnClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showEditPanel();
+        }
+    };
 
     OnClickListener clickListener = new OnClickListener() {
         @Override
@@ -70,10 +82,16 @@ public class AccountMsgItemLayout extends ConstraintLayout {
         }
     };
 
+    //显示账单详情弹窗
     private void showMsgPanel()
     {
         Intent intent = new Intent(superContext, AccountMsgActivity.class);
-        intent.putExtra("id",info.id);
+        intent.putExtra("id",info.id);//传入参数 账单id
         superContext.startActivity(intent);
+    }
+    //前往编辑账单
+    private void showEditPanel()
+    {
+        mainActivity.showAddFragment(info);
     }
 }
