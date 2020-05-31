282a9aa2f57bdee0ffa4cb6d5bb1cc77ab252d34
RM969696 <18855808066@163.com>
2020-05-31 22:27:10 +0800

    Add some hits when users click some button

--- app/src/main/java/com/example/accountbook/Helper/TipHelper.java
+++ app/src/main/java/com/example/accountbook/Helper/TipHelper.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.util.Log;
 import android.widget.Toast;
 
+//提示帮助类
 public class TipHelper {
 
     public static final String packageName = "com.example.accountbook";
@@ -26,10 +27,33 @@ public class TipHelper {
         return  value;
     }
 
+    //id未知时，根据name获取string配置
     public static void showContentTipByKey(Context context,String key)
     {
         Toast.makeText(context,getTip(context.getResources(),key),Toast.LENGTH_SHORT).show();
     }
+
+
+    public static String getContent(Resources resources,int resID)
+    {
+        String value = "";
+        try
+        {
+            value = resources.getString(resID);
+        }
+        catch (Exception e)
+        {
+            Log.e("tipError",resID + "   not found");
+        }
+        return  value;
+    }
+
+    //根据id获取string配置，并显示
+    public static void showContentTip(Context context,int resID)
+    {
+        Toast.makeText(context,getContent(context.getResources(),resID),Toast.LENGTH_SHORT).show();
+    }
+
 
 
 
